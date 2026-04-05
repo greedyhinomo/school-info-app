83cf424e4f4b86a009c4b983ce316a3dfe53e9d3
greedyhinomo <yipyiu01@gmail.com>
2026-04-05 15:56:21 +0800

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,3 +3,18 @@ document.addEventListener('deviceready', function() {
     console.log('Cordova is ready!');
     // You can add your Cordova-dependent code here.
 }, false);
+
+function loadLocalData() {
+    // Path is relative to your index.html file
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data loaded successfully:", data);
+            // Now you can use the data, for example:
+            // allSchools = data;
+            // filterAndRenderSchools();
+        })
+        .catch(error => {
+            console.error("Error loading data.json:", error);
+        });
+}
