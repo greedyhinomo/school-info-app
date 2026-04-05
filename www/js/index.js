54ae3c2f87ed0cf16d1455870902906f509206db
greedyhinomo <yipyiu01@gmail.com>
2026-04-05 13:01:49 +0800

    Create www/js/index.js for Cordova device ready

---
+++ www/js/index.js
@@ -0,0 +1,5 @@
+// Wait for Cordova to be fully loaded.
+document.addEventListener('deviceready', function() {
+    console.log('Cordova is ready!');
+    // You can add your Cordova-dependent code here.
+}, false);
